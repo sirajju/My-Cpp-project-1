25b5d9386cf0694fab0441a48d04264cf8dd5b3e
sirajju <84080512+sirajju@users.noreply.github.com>
2022-03-21 12:45:46 +0530

    Create Project.cxx

---
+++ Project.cxx
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+cin>>a>>b>>c;
+cout<<"Program Created By Sirajju";
+cout<<"Program to find the gratest number of three numbers"
+cout<<"\n Enter the first number";
+cin>>a;
+cout<<"\n Enter the second number";
+cin>>b;
+cout<<"\n Enter the third number";
+cin>>c;
+if (a>b&&b>c)
+cout<<a<<"is large \n";
+else if (b>c&&b>a)
+cout<<b<<"is large \n";
+else
+cout<<c<<"is large";
+return 0;
+}
+
